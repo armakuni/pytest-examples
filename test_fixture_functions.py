d04e1d2023dd0ea0f9e7b864ce6865352c26a332
Joe Blackman <joe.blackman@armakuni.com>
2021-11-19 14:11:30 +0100

    Demonstrate fixture functions

---
+++ test_fixture_functions.py
@@ -0,0 +1,27 @@
+import pytest
+
+
+@pytest.fixture
+def hostname():
+    return 'localhost'
+
+
+@pytest.fixture
+def port():
+    return 1234
+
+
+@pytest.fixture
+def api_path():
+    return '/api/v1'
+
+
+@pytest.fixture
+def api_url(hostname, port, api_path):
+    def construct_url(path):
+        return f"https://{hostname}:{port}{api_path}{path}"
+    return construct_url
+
+
+def test_path(api_url):
+    assert api_url("/foo/bar") == "https://localhost:1234/api/v1/foo/bar"
