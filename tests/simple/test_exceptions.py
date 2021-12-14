cf24541e7079bae891bd45132f57838a13fc4ea4
Joe Blackman <joe.blackman@armakuni.com>
2021-12-14 15:16:01 +1300

    Add example of raises(..., match)

--- tests/simple/test_exceptions.py
+++ tests/simple/test_exceptions.py
@@ -6,6 +6,18 @@ def test_exception_catching():
         do_buggy_function()
 
 
+def test_exception_catching_with_match():
+    with pytest.raises(FooError, match="[Cc]urses"):
+        do_buggy_function()
+
+
+def test_exception_catching_with_assertions():
+    with pytest.raises(FooError) as ex:
+        do_buggy_function()
+    assert ex.type is FooError
+    assert "Curses" in ex.value.args[0]
+
+
 class FooError(Exception):
     pass
 
