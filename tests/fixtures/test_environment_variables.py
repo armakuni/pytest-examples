8272dafec38f00e2d8d8bbc915b124772e5d8199
Joe Blackman <joe.blackman@armakuni.com>
2022-03-02 15:28:27 +0000

    Add example showing how to extend patched os.environ

--- tests/fixtures/test_environment_variables.py
+++ tests/fixtures/test_environment_variables.py
@@ -5,6 +5,7 @@ from unittest import mock
 
 @pytest.fixture
 def env_creds():
+    # patched os.environ that will not pollute other tests
     with mock.patch.dict(
         os.environ, {"CRED_USERNAME": "fred", "CRED_PASSWORD": "secret"}
     ):
@@ -19,3 +20,14 @@ def test_creds(env_creds):
 def test_creds_unset():
     assert os.environ.get("CRED_USERNAME") is None
     assert os.environ.get("CRED_PASSWORD") is None
+
+
+@pytest.fixture
+def more_creds(env_creds):
+    # pre-patched os.environ can be extended
+    os.environ['EXTRA_CRED'] = 'someothersecret'
+
+
+def test_more_environment_variables(more_creds):
+    assert os.environ.get('EXTRA_CRED') == 'someothersecret'
+    assert os.environ.get("CRED_USERNAME") == "fred"
